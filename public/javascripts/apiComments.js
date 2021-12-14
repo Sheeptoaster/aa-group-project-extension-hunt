9509be618382238d364952bd456a4cafbd544fdc
nathaniel <nathanieltseng@gmail.com>
2021-12-14 13:58:42 -0800

    #23 comment validations and dom manipulation for errors

--- public/javascripts/apiComments.js
+++ public/javascripts/apiComments.js
@@ -1,24 +1,27 @@
-document.querySelector("#create-comment").addEventListener("click", async (event => {
+document.querySelector("#create-comment").addEventListener("click", async(event => {
 	// Call POST /api/comments
-	// let commentInput = document.querySelector("#comment-input");
 	const addCommentForm = document.querySelector('#add-comment-form')
 	const formData = new FormData(addCommentForm)
 	const content = formData.get('content')
 	const extensionId = formData.get('extensionId')
-	const userId = formData.get('userId')
 	let res = await fetch("/api/comments", {
 		method: "POST",
 		header: "application/json",
 		body: {
 			content,
-			extensionId,
-			userId
+			extensionId
 		}
 	})
 
-	// const { content, userId } = await res.json();
-	const contentContainer = document.querySelector('#content-container')
-	const div = document.createElement('div')
-	div.appendChild(`<h1>${userId}, ${content}</h1>`)
-	contentContainer.appendChild(div)
+	const data = await res.json();
+	if (!data.error) {
+		const { username } = data;
+		const contentContainer = document.querySelector('#content-container')
+		const div = document.createElement('div') //TODO replace with styled comment html
+		div.appendChild(`<h1>${username}, ${content}</h1>`)
+		contentContainer.appendChild(div)
+	} else {
+		// display errors dynamically
+		document.querySelector("#comment-errors").appendChild(data.error);
+	}
 }))
