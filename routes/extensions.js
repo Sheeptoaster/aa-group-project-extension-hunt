67824e33acfb5539585dae1d8ef368f36a4bd4a1
nathaniel <nathanieltseng@gmail.com>
2021-12-14 15:57:22 -0800

    #27 GET /products/:id

--- routes/extensions.js
+++ routes/extensions.js
@@ -8,15 +8,23 @@ const { check, validationResult } = require('express-validator');
 
 const router = express.Router()
 
-router.get('/new', csrfProtection, asyncHandler(async(req, res) => {
-    const categories = await db.Category.findAll()
-    res.render('create-extension', { csrfToken: req.csrfToken(), title: "Create New Extension", categories })
+router.get('/new', csrfProtection, asyncHandler(async (req, res) => {
+	const categories = await db.Category.findAll()
+	res.render('create-extension', { csrfToken: req.csrfToken(), title: "Create New Extension", categories })
 }))
 
 
-router.get('/:id', (req, res) => {
-
-})
+router.get('/:id(\\d+)', asyncHandler(async (req, res) => {
+	const extensionId = parseInt(req.params.id);
+	const extension = await db.Extension.findByPk(extensionId);
+	const comments = await db.Comment.findAll({ where: { extensionId } })
+	res.render("extension", {
+		name: extension.name,
+		descrption: extension.descrption,
+		iconURL: extension.iconURL,
+		comments //TODO #56 visual design of comments
+	});
+}))
 
 
 
