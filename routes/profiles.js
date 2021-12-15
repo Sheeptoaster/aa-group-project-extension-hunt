e673131e05a1ecb420b1941a011f33c01d4315c1
Jake <jweber04005@msn.com>
2021-12-15 12:39:15 -0500

    Edit User Profile Page

--- routes/profiles.js
+++ routes/profiles.js
@@ -9,17 +9,47 @@ const router = express.Router()
 
 router.get('/:id', asyncHandler(async(req, res) => {
     const userId = parseInt(req.params.id)
-    const user = await db.User.findOne({
+    const profileUser = await db.User.findOne({
         where: {
             id: userId
         },
         include: [ db.Comment, db.Extension ]
     })
-    let extensionNames = user.Extensions.map(extension => extension.name)
-    console.log(user.Extensions[0].name)
-    res.render('profile-page', { title: "Profile Page", user, req, extensionNames })
+
+
+    let extensionNames = profileUser.Extensions.map(extension => extension.name)
+
+    res.render('profile-page', { title: "Profile Page", profileUser, req, extensionNames })
 }))
 
+
+router.get('/:id/edit', csrfProtection, asyncHandler(async(req, res) => {
+    const userId = parseInt(req.params.id);
+    const currentUserId = req.session.auth.userId;
+
+
+    if(userId === currentUserId) {
+        res.render('profile-edit', { title: 'Edit Profile', csrfToken: req.csrfToken() })
+    } else {
+        res.redirect(`/profiles/${userId}`)
+    }
+}))
+
+router.patch('/:id/edit', csrfProtection, asyncHandler(async(req, res) => {
+    const userId = req.params.id;
+    const profileUser = await db.User.findByPk(userId)
+
+    const {
+        firstName,
+        lastName,
+        username,
+        email,
+        bio,
+        avatarURL,
+        password,
+        confirmPassword
+    } = req.body
+}))
 
 
 module.exports = router
