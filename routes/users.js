8ec16f77ae5169730e49eeacda178e1f23dec055
Jake W <85136034+Sheeptoaster@users.noreply.github.com>
2021-12-16 11:17:57 -0500

    Update users.js

--- routes/users.js
+++ routes/users.js
@@ -78,7 +78,7 @@ let signUpValidation = [
 		.exists({ checkFalsy: true })
 		.withMessage('Please provide a username')
 		.isLength({ max: 50 })
-		.withMessage('username must be shorter than 50 characters')
+		.withMessage('Username must be shorter than 50 characters')
 		.custom(value => {
 			return db.User.findOne({ where: { username: value } }).then(previousUser => {
 				if (previousUser) {
@@ -92,19 +92,19 @@ let signUpValidation = [
 		.exists({ checkFalsy: true })
 		.withMessage('Please provide a first name')
 		.isLength({ max: 50 })
-		.withMessage('first name must be shorter than 50 characters'),
+		.withMessage('First name must be shorter than 50 characters'),
 	check('lastName')
 		.exists({ checkFalsy: true })
 		.withMessage('Please provide a last name')
 		.isLength({ max: 50 })
-		.withMessage('last name must be shorter than 50 characters'),
+		.withMessage('Last name must be shorter than 50 characters'),
 	check('email')
 		.exists({ checkFalsy: true })
 		.withMessage('Please provide a email')
 		.isEmail()
 		.withMessage('Please provide a valid email')
 		.isLength({ max: 100 })
-		.withMessage('email must be shorter than 100 characters')
+		.withMessage('Email must be shorter than 100 characters')
 		.custom(value => {
 			return db.User.findOne({ where: { email: value } }).then(previousUser => {
 				if (previousUser) {
