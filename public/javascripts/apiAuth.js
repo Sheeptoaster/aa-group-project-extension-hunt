4d63e7af79ac69ecc54e97afd9d1969fbaba5b94
nathaniel <nathanieltseng@gmail.com>
2021-12-17 14:15:28 -0800

    - fixed adding event listener to non-existent component (demo signin) - fixed html in nav-bar-right after user login - fixed upvote buttons beyond first on home page

--- public/javascripts/apiAuth.js
+++ public/javascripts/apiAuth.js
@@ -2,6 +2,7 @@ const loginButton = document.querySelector('#login-button')
 const cancelPopupButton = document.querySelector("#cancel-popup-button")
 const loginBackground = document.querySelector("#login-popup-background");
 const loginSignup = document.querySelector('#login-from-signup')
+const demoSignin = document.querySelector("#demo-sign-in");
 
 if (loginSignup) {
 	loginSignup.addEventListener("click", async event => {
@@ -54,13 +55,12 @@ document.querySelector("#login-submit").addEventListener("click", async event =>
 		//DOM manipulate login and signout. replace with logout
 		const welcomeContainer = document.querySelector('#nav-bar-right')
 		welcomeContainer.innerHTML = `
-            <div>
-                <span> Welcome ${data.user.firstName} </span>
-                <form action="/users/logout" method="POST">
-                    <button type="submit">Logout</button>
-                </form>
-            </div>
-        `
+		<span>Welcome ${data.user.firstName}</span>
+		<a href="/profiles/${data.user.id}">Profile</a>
+		<form action="/users/logout" method="POST">
+			<button type="submit">Logout</button>
+		</form>
+	`
 		//hide the login popup again
 		const popupElement = document.querySelector('#login-popup')
 		popupElement.classList.add("hidden");
@@ -74,33 +74,35 @@ document.querySelector("#login-submit").addEventListener("click", async event =>
 	}
 })
 
-document.querySelector("#demo-sign-in").addEventListener("click", async event => {
-	event.preventDefault()
-	const loginForm = document.querySelector('#login-form')
-	const loginData = new FormData(loginForm)
-	const csrf = loginData.get('_csrf')
-	let res = await fetch('/api/auth/login', {
-		method: 'POST',
-		headers: {
-			"Content-Type": "application/json"
-		},
-		body: JSON.stringify({
-			username: "Demo User",
-			password: "a",
-			_csrf: csrf
+if (demoSignin) {
+	demoSignin.addEventListener("click", async event => {
+		event.preventDefault()
+		const loginForm = document.querySelector('#login-form')
+		const loginData = new FormData(loginForm)
+		const csrf = loginData.get('_csrf')
+		let res = await fetch('/api/auth/login', {
+			method: 'POST',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				username: "Demo User",
+				password: "a",
+				_csrf: csrf
+			})
 		})
-	})
-	const data = await res.json()
+		const data = await res.json()
 
-	if (!data.errors) {
-		//DOM manipulate login and signout. replace with logout
-		const welcomeContainer = document.querySelector('#nav-bar-right')
-		welcomeContainer.innerHTML = `
+		if (!data.errors) {
+			//DOM manipulate login and signout. replace with logout
+			const welcomeContainer = document.querySelector('#nav-bar-right')
+			welcomeContainer.innerHTML = `
 			<span>Welcome ${data.user.firstName}</span>
 			<a href="/profiles/51">Profile</a>
 			<form action="/users/logout" method="POST">
-				<button type="submit">Logout</button>
+			<button type="submit">Logout</button>
 			</form>
-        `
-	}
-})
+			`
+		}
+	})
+}
--- public/javascripts/apiUpvote.js
+++ public/javascripts/apiUpvote.js
@@ -1,20 +1,20 @@
-const upvoteButton = document.querySelector('.upvote-container')
+const upvoteButtons = document.querySelectorAll('.upvote-container')
 
-upvoteButton.addEventListener('click', async (event) => {
+upvoteButtons.forEach(button => {
+	button.addEventListener('click', async (event) => {
+		const extensionId = button.getAttribute('extensionId')
 
-    const extensionId = upvoteButton.getAttribute('extensionId')
-
-    const res = await fetch('/api/upvote', {
-        method: 'PATCH',
-        headers: {
-			"Content-Type": "application/json"
-		},
-        body: JSON.stringify({
-            extensionId
-        })
-    })
-    const data = await res.json()
-    const upvoteElement = document.getElementById(`${extensionId}`)
-    upvoteElement.innerText = data.upvotes
-
+		const res = await fetch('/api/upvote', {
+			method: 'PATCH',
+			headers: {
+				"Content-Type": "application/json"
+			},
+			body: JSON.stringify({
+				extensionId
+			})
+		})
+		const data = await res.json()
+		const upvoteElement = document.getElementById(`${extensionId}`)
+		upvoteElement.innerText = data.upvotes
+	})
 })
