a7ba7783b6a99a18c8d437d6d148cc1ea129b01f
Chris Young <ChrisC.Young1@gmail.com>
2021-12-17 09:28:58 -0800

    moved a link so that upvote button doesnt redirect

---
+++ public/javascripts/apiUpvote.js
@@ -0,0 +1,6 @@
+const upvoteButton = document.querySelector('.upvote-container')
+
+upvoteButton.addEventListener('click', async (event) => {
+    // event.preventDefault();
+    console.log(event)
+})
---
+++ routes/apiUpvote.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const db = require('../db/models');
+const { asyncHandler } = require('./utils');
+
+
+
+const router = express.Router()
+
+router.patch('/', asyncHandler(async (req, res) => {
+    
+}))
+
+
+
+
+
+
+module.exports = router
