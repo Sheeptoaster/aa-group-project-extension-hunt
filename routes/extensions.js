f6e513aff86f664c246f3be478ac0e8378dd9d2e
nathaniel <nathanieltseng@gmail.com>
2021-12-19 11:31:06 -0800

    - reworked categoryId fetching from checkboxes (new and edit) - fixed bad arguments for pug (which caused 404s) - fixed editing of extension category list (which also caused 404s) - extension edit page now prefills with values - extension edit page now lists which extension is being edited - exention edit page now redirects to the edited extension's page - cleaned out some comments from users route

--- routes/extensions.js
+++ routes/extensions.js
@@ -3,146 +3,156 @@ const db = require('../db/models')
 const { csrfProtection, asyncHandler } = require('./utils')
 const { requireAuth } = require('../auth');
 const { check, validationResult } = require('express-validator');
-const e = require('express');
 
 const router = express.Router()
 
 router.get('/new', requireAuth, csrfProtection, asyncHandler(async (req, res) => {
-    //Finds All Categories
-    const categories = await db.Category.findAll()
-    //Renders Create Extension Page
-    res.render('create-extension', { csrfToken: req.csrfToken(), title: "Create New Extension", categories })
+	//Finds All Categories
+	const categories = await db.Category.findAll()
+	//Renders Create Extension Page
+	res.render('create-extension', { csrfToken: req.csrfToken(), title: "Create New Extension", categories })
 }))
 
 const extensionValidation = [
-    check("newName")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide an Extension Name")
-        .isLength({ max: 50 })
-        .withMessage("Extension Name must be shorter than 50 characters"),
-    check("newDescription")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a description")
-];
-
-const updateExtensionValidation = [
-    check("editName")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide an Extension Name")
-        .isLength({ max: 50 })
-        .withMessage("Extension Name must be shorter than 50 characters"),
-    check("editDescription")
-        .exists({ checkFalsy: true })
-        .withMessage("Please provide a description")
+	check("newName")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide an Extension Name")
+		.isLength({ max: 50 })
+		.withMessage("Extension Name must be shorter than 50 characters"),
+	check("newDescription")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a description")
 ];
 
 router.post('/new', csrfProtection, extensionValidation, asyncHandler(async (req, res) => {
-    const {
-        newName,
-        newDescription,
-        newIconURL,
-        newSlogan,
-        categoriesCheckboxes
-    } = req.body;
-    console.log(req.body);
-    const ownerId = req.session.auth.userId;
-    const extension = await db.Extension.build({ name: newName, description: newDescription, iconURL: newIconURL, slogan: newSlogan, upvotes: 0, ownerId });
+	const {
+		newName,
+		newDescription,
+		newIconURL,
+		newSlogan,
+		categoryIds
+	} = req.body;
+	const ownerId = req.session.auth.userId;
+	const extension = await db.Extension.build({ name: newName, description: newDescription, iconURL: newIconURL, slogan: newSlogan, upvotes: 0, ownerId });
 
-    const validatorErrors = validationResult(req)
-    const categoryValues = Object.values(req.body);
-    const categoryId = categoryValues.slice(5);
-    if (validatorErrors.isEmpty()) {
-        await extension.save();
-        for (let i = 0; i < categoryId.length; i++) {
-            await db.ExtensionCategories.create({ extensionId: extension.id, categoryId: categoryId[i] })
-        }
-        res.redirect('/')
-    } else {
-        const categories = await db.Category.findAll()
-        const errors = (validatorErrors.array().map(error => error.msg));//TODO #66 catch sequelize unique errors
-        res.render("create-extension", { errors, csrfToken: req.csrfToken(), title: "Create New Extension", categories });
-    }
+	const validatorErrors = validationResult(req)
+	if (validatorErrors.isEmpty()) {
+		await extension.save();
+		for (let i = 0; i < categoryIds.length; i++) {
+			await db.ExtensionCategories.create({ extensionId: extension.id, categoryId: categoryIds[i] })
+		}
+		res.redirect('/')
+	} else {
+		const categories = await db.Category.findAll();
+		const errors = validatorErrors.array().map(error => error.msg);
+		res.render("create-extension", { errors, csrfToken: req.csrfToken(), title: "Create New Extension", categories });
+	}
 }))
 
 router.get('/:id(\\d+)', csrfProtection, asyncHandler(async (req, res) => {
-    const extensionId = parseInt(req.params.id);
-    const extension = await db.Extension.findByPk(extensionId);
-    const comments = await db.Comment.findAll({
-        where: { extensionId },
-        include: {
-            model: db.User,
-            as: "User"
-        }
-    })
-    res.render("extension", {
-        extension,
-        comments,
-        csrfToken: req.csrfToken()
-    });
+	const extensionId = parseInt(req.params.id);
+	const extension = await db.Extension.findByPk(extensionId);
+	const comments = await db.Comment.findAll({
+		where: { extensionId },
+		include: {
+			model: db.User,
+			as: "User"
+		}
+	})
+	res.render("extension", {
+		extension,
+		comments,
+		csrfToken: req.csrfToken()
+	});
 }))
 
 router.get('/:id(\\d+)/edit', csrfProtection, asyncHandler(async (req, res) => {
-    const extension = parseInt(req.params.id);
+	let extensionId = parseInt(req.params.id);
+	const extension = await db.Extension.findByPk(extensionId);
+	const categories = await db.Category.findAll();
+	const extensionCategories = await db.ExtensionCategories.findAll({
+		where: { extensionId }
+	})
+	let categoryIds = extensionCategories.map((category) => category.categoryId)
 
-    const extensions = await db.Extension.findByPk(extension)
-
-    const categories = await db.ExtensionCategories.findAll({
-        where: {
-            extensionId: extension
-        }
-    })
-    let categoryId = categories.map((category) => category.categoryId)
-
-    const categoriesName = await db.Category.findAll()
-
-    res.render('extension-edit', { title: 'Edit Extension', csrfToken: req.csrfToken(), extensions, categoriesName, categoryId })
+	res.render('extension-edit', {
+		errors: [],
+		csrfToken: req.csrfToken(),
+		id: extension.id,
+		extensionName: extension.name,
+		description: extension.description,
+		iconURL: extension.iconURL,
+		slogan: extension.slogan,
+		categories,
+		categoryIds
+	})
 }))
 
+const updateExtensionValidation = [
+	check("editName")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide an Extension Name")
+		.isLength({ max: 50 })
+		.withMessage("Extension Name must be shorter than 50 characters"),
+	check("editDescription")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a description")
+];
+
+//TODONOW checkboxes have duplicate id and value attributes
 router.post('/:id(\\d+)/edit', csrfProtection, updateExtensionValidation, asyncHandler(async (req, res) => {
-    const { editName, editDescription, editIconURL, editSlogan, categoriesCheckboxes } = req.body
-    const extensionId = parseInt(req.params.id)
+	const { editName, editDescription, editIconURL, editSlogan, categoryIds } = req.body;
+	const extensionId = parseInt(req.params.id);
+	const extension = await db.Extension.findByPk(extensionId);
 
+	const validatorErrors = validationResult(req);
+	if (validatorErrors.isEmpty()) {
+		await extension.update({
+			name: editName,
+			description: editDescription,
+			iconURL: editIconURL,
+			slogan: editSlogan,
+		})
+		await extension.save()
 
-    const extension = await db.Extension.findByPk(extensionId)
-
-    const categoryValues = Object.values(req.body)
-    const selectedCategoryId = categoryValues.slice(5)
-
-    const validatorErrors = validationResult(req)
-
-    if (validatorErrors.isEmpty()) {
-        const updatedExtension = await extension.update({
-            name: editName,
-            description: editDescription,
-            iconURL: editIconURL,
-            slogan: editSlogan,
-        })
-        await extension.save()
-        for (let i = 0; i < selectedCategoryId.length; i++) {
-            const extensionCategory = await db.ExtensionCategories.findByPk(parseInt(selectedCategoryId[i]))
-            const updatedExtensionCategory = await extensionCategory.update({
-                extensionId: updatedExtension.id,
-                categoryId: selectedCategoryId[i]
-            })
-            console.log(updatedExtensionCategory.id);
-            await updatedExtensionCategory.save()
-        }
-        console.log('passed--------------------------');
-        res.redirect('/');
-    } else {
-        const errors = (validatorErrors.array().map(error => error.msg));
-        console.log('failed--------------------------');
-        res.render('extension-edit', { errors, csrfToken: req.csrfToken(), editName, editDescription, editIconURL, editSlogan, categoriesCheckboxes })
-    }
+		// Update categories
+		const extensionCategories = await db.ExtensionCategories.findAll({
+			where: { extensionId }
+		})
+		extensionCategories.forEach(async row => {
+			await row.destroy();
+		})
+		if (categoryIds) {
+			for (const categoryId of categoryIds) {
+				console.log(categoryId);
+				await db.ExtensionCategories.create({ extensionId, categoryId })
+			}
+		}
+		res.redirect(`/extensions/${extensionId}`);
+	} else {
+		const errors = validatorErrors.array().map(error => error.msg);
+		const categories = await db.Category.findAll();
+		res.render('extension-edit', {
+			errors,
+			csrfToken: req.csrfToken(),
+			id: extensionId,
+			extensionName: editName || extension.name,
+			description: editDescription || extension.description,
+			iconURL: editIconURL,
+			slogan: editSlogan,
+			categories,
+			categoryIds: categoryIds || []
+		})
+	}
 }))
 
 router.post('/:id(\\d+)/delete', asyncHandler(async (req, res) => {
-    const extensionId = parseInt(req.params.id)
+	const extensionId = parseInt(req.params.id)
 
-    const deletedExtension = await db.Extension.findByPk(extensionId)
-    await deletedExtension.destroy()
+	const deletedExtension = await db.Extension.findByPk(extensionId)
+	await deletedExtension.destroy()
 
-    res.redirect('/')
+	res.redirect('/')
 }))
 
 module.exports = router
