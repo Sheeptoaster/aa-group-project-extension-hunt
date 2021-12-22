06022982e88308161fbee2981b9d46f0557799ab
Chris Young <ChrisC.Young1@gmail.com>
2021-12-22 12:03:07 -0800

    Got extension page upvote button working, but couldnt test while not logged in

--- public/javascripts/apiUpvote.js
+++ public/javascripts/apiUpvote.js
@@ -1,4 +1,5 @@
 const upvoteButtons = document.querySelectorAll('.upvote-container')
+const extensionUpvote = document.querySelector('#upvote-btn-extension')
 
 upvoteButtons.forEach(button => {
 	button.addEventListener('click', async (event) => {
@@ -19,4 +20,24 @@ upvoteButtons.forEach(button => {
 			upvoteElement.innerText = data.upvotes
 		}
 	})
+})
+
+extensionUpvote.addEventListener('click', async (event) => {
+	const extensionId = extensionUpvote.getAttribute('extensionId')
+
+	const res = await fetch('/api/upvote', {
+		method: 'PATCH',
+		headers: {
+			"Content-Type": "application/json"
+		},
+		body: JSON.stringify({
+			extensionId
+		})
+	})
+
+	const data = await res.json()
+	if (data.upvotes) {
+		const upvoteElement = document.querySelector(`.extension-upvotes`)
+		upvoteElement.innerText = data.upvotes
+	}
 })
