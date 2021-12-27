209c01b6212f8a3db85e0a755bc12ff22dc7f965
nathaniel <nathanieltseng@gmail.com>
2021-12-26 16:35:01 -0800

    fix crash on submit edit user

--- routes/profiles.js
+++ routes/profiles.js
@@ -2,116 +2,116 @@ const express = require("express");
 const db = require("../db/models");
 const { csrfProtection, asyncHandler } = require("./utils");
 const { check, validationResult } = require("express-validator");
-const { requireAuth } = require("../auth");
 
 const router = express.Router();
 
 const userUpdateValidation = [
-  check("username")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a username")
-    .isLength({ max: 50 })
-    .withMessage("username must be shorter than 50 characters"),
-  check("firstName")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a first name")
-    .isLength({ max: 50 })
-    .withMessage("first name must be shorter than 50 characters"),
-  check("lastName")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a last name")
-    .isLength({ max: 50 })
-    .withMessage("last name must be shorter than 50 characters"),
-  check("email")
-    .exists({ checkFalsy: true })
-    .withMessage("Please provide a email")
-    .isEmail()
-    .withMessage("Please provide a valid email")
-    .isLength({ max: 100 })
-    .withMessage("email must be shorter than 100 characters"),
+	check("editUsername")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a username")
+		.isLength({ max: 50 })
+		.withMessage("username must be shorter than 50 characters"),
+	check("editFirstName")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a first name")
+		.isLength({ max: 50 })
+		.withMessage("first name must be shorter than 50 characters"),
+	check("editLastName")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a last name")
+		.isLength({ max: 50 })
+		.withMessage("last name must be shorter than 50 characters"),
+	check("editEmail")
+		.exists({ checkFalsy: true })
+		.withMessage("Please provide a email")
+		.isEmail()
+		.withMessage("Please provide a valid email")
+		.isLength({ max: 100 })
+		.withMessage("email must be shorter than 100 characters"),
 ];
 
 router.get(
-  "/:id",
-  csrfProtection,
-  asyncHandler(async (req, res) => {
-    const userId = parseInt(req.params.id);
-    const profileUser = await db.User.findOne({
-      where: {
-        id: userId,
-      },
-      include: [db.Comment, { model: db.Extension, include: db.Category }],
-    });
-    let extensionNames = profileUser.Extensions.map(
-      (extension) => extension.name
-    );
+	"/:id",
+	csrfProtection,
+	asyncHandler(async (req, res) => {
+		const userId = parseInt(req.params.id);
+		const profileUser = await db.User.findOne({
+			where: {
+				id: userId,
+			},
+			include: [db.Comment, { model: db.Extension, include: db.Category }],
+		});
+		let extensionNames = profileUser.Extensions.map(
+			(extension) => extension.name
+		);
 
-    let extensions = profileUser.Extensions
-    let comments = profileUser.Comments
+		let extensions = profileUser.Extensions
+		let comments = profileUser.Comments
 
-    res.render('profile-page', { title: "Profile Page", profileUser, req, extensions, comments, csrfToken: req.csrfToken(), authenticated: !!res?.locals.authenticated })
-  })
+		res.render('profile-page', { title: "Profile Page", profileUser, req, extensions, comments, csrfToken: req.csrfToken(), authenticated: !!res?.locals.authenticated })
+	})
 );
 
 router.get(
-  "/:id/edit",
-  csrfProtection,
-  asyncHandler(async (req, res) => {
-    const userId = parseInt(req.params.id);
-    const currentUserId = req.session.auth.userId;
+	"/:id/edit",
+	csrfProtection,
+	asyncHandler(async (req, res) => {
+		const userId = parseInt(req.params.id);
+		const currentUserId = req.session.auth.userId;
 
-    const profileUser = await db.User.findByPk(userId);
+		const profileUser = await db.User.findByPk(userId);
 
-    if (userId === currentUserId) {
-      res.render("profile-edit", {
-        title: "Edit Profile",
-        profileUser,
-        csrfToken: req.csrfToken(),
-      });
-    } else {
-      res.redirect(`/profiles/${userId}`);
-    }
-  })
+		if (userId === currentUserId) {
+			res.render("profile-edit", {
+				title: "Edit Profile",
+				profileUser,
+				csrfToken: req.csrfToken(),
+			});
+		} else {
+			res.redirect(`/profiles/${userId}`);
+		}
+	})
 );
 
 router.post(
-  "/:id/edit",
-  csrfProtection,
-  userUpdateValidation,
-  asyncHandler(async (req, res) => {
-    const userId = req.params.id;
-    const profileUser = await db.User.findByPk(userId);
+	"/:id/edit",
+	csrfProtection,
+	userUpdateValidation,
+	asyncHandler(async (req, res) => {
+		const userId = req.params.id;
+		const profileUser = await db.User.findByPk(userId);
+		const { editFirstName, editLastName, editUsername, editEmail, editBio, editAvatarURL } = req.body;
 
-    const { editFirstName, editLastName, editUsername, editEmail, editBio, editAvatarURL } = req.body;
+		const validatorErrors = validationResult(req);
 
-    const validatorErrors = validationResult(req);
+		if (validatorErrors.isEmpty()) {
+			const updatedUser = await profileUser.update({
+				firstName: editFirstName,
+				lastName: editLastName,
+				username: editUsername,
+				email: editEmail,
+				bio: editBio,
+				avatarURL: editAvatarURL,
+			});
 
-    if (validatorErrors.isEmpty()) {
-      const updatedUser = await profileUser.update({
-        firstName: editFirstName,
-        lastName: editLastName,
-        username: editUsername,
-        email: editEmail,
-        bio: editBio,
-        avatarURL: editAvatarURL,
-      });
-
-      res.redirect(`/profiles/${userId}`);
-    } else {
-        const errors = validatorErrors.array().map(error => error.msg);
-        res.render('profile-edit', {
-            errors,
-            firstName,
-            lastName,
-            username,
-            email,
-            bio,
-            avatarURL,
-            profileUser,
-            csrfToken: req.csrfToken()
-        })
-    }
-  })
+			res.redirect(`/profiles/${userId}`);
+		} else {
+			const errors = validatorErrors.array().map(error => error.msg);
+			res.render('profile-edit', {
+				errors,
+				profileUser: {
+					id: profileUser.id,
+					username: editUsername || profileUser.username,
+					firstName: editFirstName || profileUser.firstName,
+					lastName: editLastName || profileUser.lastName,
+					email: editEmail || profileUser.email,
+					bio: editBio || profileUser.bio,
+					avatarURL: editAvatarURL || profileUser.avatarURL
+				},
+				csrfToken: req.csrfToken()
+			})
+		}
+	})
 );
 
 module.exports = router;
